75b3b67f805448d7ebbf6c4257352d7fe223c800
LimaUni <123000307+LimaUni@users.noreply.github.com>
2026-03-01 02:31:40 -0600

    Examen parcial 1 practica
    Nombre: Javier Lima
    Carne: 9941-22-9790
    Seccion: B
    Explicacion: En este programa se da un ejemplo de como se usan correctamente los nodos, punteros y liberacion de memoria. se observa que se le pide al usuario un valor por cada nodo y al final se muestra en una lista todos los datos asignados para luego ser borrados y liberar la memoria.

---
+++ Parcial_I_Practica/main.cpp
@@ -0,0 +1,53 @@
+//Nombre: Javier Lima
+//Carne: 9941-22-9790
+//Curso: Programacion III
+//Semana V - Examen Parcial 1 Practica
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Nodo {
+    int dato;
+    Nodo* siguiente;
+};
+
+int main() {
+    string nombre = "Jeremy Javier Lima Guitron";
+
+    Nodo* nodo1 = new Nodo();
+    Nodo* nodo2 = new Nodo();
+    Nodo* nodo3 = new Nodo();
+
+    cout << "Ingrese el valor para el primer nodo: ";
+    cin >> nodo1->dato;
+
+    cout << "Ingrese el valor para el segundo nodo: ";
+    cin >> nodo2->dato;
+
+    cout << "Ingrese el valor para el tercer nodo: ";
+    cin >> nodo3->dato;
+
+    nodo1->siguiente = nodo2;
+    nodo2->siguiente = nodo3;
+    nodo3->siguiente = nullptr;
+
+    cout << "\n-----------------------------------" << endl;
+    cout << "Nombre:: " << nombre << endl;
+    cout << "-----------------------------------" << endl;
+    cout << "Valores almacenados en la lista:" << endl;
+
+    cout << "Nodo 1: " << nodo1->dato << endl;
+    cout << "Nodo 2: " << nodo1->siguiente->dato << endl;
+    cout << "Nodo 3: " << nodo1->siguiente->siguiente->dato << endl;
+    cout << "-----------------------------------\n" << endl;
+
+    delete nodo1;
+    delete nodo2;
+    delete nodo3;
+
+    cout << "Memoria liberada exitosamente." << endl;
+
+    return 0;
+}
